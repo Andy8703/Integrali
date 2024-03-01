98247935c9f7dd10913f8a625f6e862461b2916c
Andy8703 <andreriv87@gmail.com>
2024-03-01 09:36:13 +0100

    Aggiunta ciclo for

--- main.c
+++ main.c
@@ -20,6 +20,7 @@
 
 #include <stdio.h> 
 #include <stdlib.h>
+#include <time.h>
 #include "mathf.h"
 
  /*! \brief the main function
@@ -29,48 +30,52 @@
 
  */
 int main() {
-
+	srand(time(NULL));
 	/* setting all the parameters */
 	float fcoeff[4] = { -10.0,1.0,0.0,2.0 };	/* the polynomial (-10 + x + 2x^3) */
-	float xmin = 0.0; 						/* the integration range */
-	float xmax = 5.0;
-	int intervals = 500; 					/* number of equally spaced intervals */
-
 
 	float integ1, integ2;
 
-	int i;
-	float in = xmin;
-	float gap = (xmax - xmin) / (float)intervals;
-	float* fvalues = NULL;
+	for (int j = 0; j < 10; j++)
+	{
+		float xmin = rand()%100; 						/* the integration range */
+		float xmax = rand()%100 + 100;
+		int intervals = rand() % 1000000;
+		int i;
+		float in = xmin;
+		float gap = (xmax - xmin) / (float)intervals;
+		float* fvalues = NULL;
 
-	/* this array will store the (steps+1) values of the polynomial that are delimiting the equally spaced intervals*/
-	fvalues = (float*)malloc(sizeof(float) * (intervals + 1));
-	if (fvalues == NULL) {
-		printf("\nERROR: cannot allocate memory\n");
-		exit(-1);
-	}
+		/* this array will store the (steps+1) values of the polynomial that are delimiting the equally spaced intervals*/
+		fvalues = (float*)malloc(sizeof(float) * (intervals + 1));
+		if (fvalues == NULL) {
+			printf("\nERROR: cannot allocate memory\n");
+			exit(-1);
+		}
 
-	/* to get the value of the polynomial at the different points that are delimiting the intervals */
-	for (i = 0; i <= intervals; i++) {
-		fvalues[i] = Polynomial(fcoeff, 4, in);
-		in += gap;
-	}
+		/* to get the value of the polynomial at the different points that are delimiting the intervals */
+		for (i = 0; i <= intervals; i++) {
+			fvalues[i] = Polynomial(fcoeff, 4, in);
+			in += gap;
+		}
 
-	integ1 = 0.;
-	integ2 = 0.;
+		integ1 = 0.;
+		integ2 = 0.;
 
-	/* to compute the integral according to the rectangular rule */
-	Rectangular(fvalues, intervals + 1, gap, &integ1, &integ2);
-	printf("\nRectangular rule - The integral between %f and %f is in the interval: [%f,%f]\n", xmin, xmax, integ1, integ2);
+		/* to compute the integral according to the rectangular rule */
+		Rectangular(fvalues, intervals + 1, gap, &integ1, &integ2);
+		printf("\nRectangular rule - The integral between %f and %f is in the interval: [%f,%f]\n", xmin, xmax, integ1, integ2);
 
-	/* to compute the integral according to the trapezoidal rule */
-	integ1 = Trapezoidal(fvalues, intervals + 1, gap);
-	printf("\nTrapezoidal rule - The integral between %f and %f is : %f\n", xmin, xmax, integ1);
+		/* to compute the integral according to the trapezoidal rule */
+		integ1 = Trapezoidal(fvalues, intervals + 1, gap);
+		printf("\nTrapezoidal rule - The integral between %f and %f is : %f\n", xmin, xmax, integ1);
 
-	/* deallocation */
-	if (fvalues != NULL)
-		free(fvalues);
+		printf("%i\n", intervals);
+
+		/* deallocation */
+		if (fvalues != NULL)
+			free(fvalues);
+	}	
 
 	return 0;
 }
